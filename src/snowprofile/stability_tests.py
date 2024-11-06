d6f208a99d48cff7308fcd2413b6102dcd95da71
Leo VIALLON-GALINIER <leo.viallon@meteo.fr>
2024-11-06 16:20:04 +0100

    First draft stability tests

--- src/snowprofile/stability_tests.py
+++ src/snowprofile/stability_tests.py
@@ -11,3 +11,33 @@ class _StabilityTest(pydantic.BaseModel):
         extra='forbid')
 
     id: typing.Optional[str] = None
+
+
+class RBStabilityTest(_StabilityTest):
+    """
+    Class for RB (Rutschblock) stability test.
+    """
+
+
+class CTStabilityTest(_StabilityTest):
+    """
+    Class for CT (Compression test) stability test.
+    """
+
+
+class ECTStabilityTest(_StabilityTest):
+    """
+    Class for ECT (Extended column test) stability test.
+    """
+
+
+class PSTStabilityTest(_StabilityTest):
+    """
+    Class for PST (Propagation Saw test) stability test.
+    """
+
+
+class ShearFrameStabilityTest(_StabilityTest):
+    """
+    Class for Shear frame stability test.
+    """
