0cfabe2095962f402afc2fe8691cb94c63875ce5
Leo VIALLON-GALINIER <leo.viallon@meteo.fr>
2024-12-18 15:43:57 +0100

    Dev: Stability tests

--- src/snowprofile/stability_tests.py
+++ src/snowprofile/stability_tests.py
@@ -72,9 +72,9 @@ class RBStabilityTestResult(_StabilityTestResult):
         validate_assignment=True,
         extra='forbid')
 
-    test_score: typing.Literal[
-        'RB1', 'RB2', 'RB3', 'RB4', 'RB5', 'RB6', 'RB7'] = pydantic.Field(
-            description="RB result RB[1-7], RB7 meaning no fracture")
+    test_score: int = pydantic.Field(
+        description="RB result [1-7], 7 meaning no fracture",
+        ge=0, le=7)
     release_type: typing.Optional[typing.Literal[
         'WB', 'MB', 'EB']] = pydantic.Field(
             None,
@@ -99,25 +99,95 @@ class RBStabilityTest(_StabilityTest):
         description="Successive results of a single RB test. No results mean RB7.")
 
 
+class CTStabilityTestResult(_StabilityTestResult):
+    """
+    Class for CT (Comprerssion test) stability test results.
+    """
+    # TODO: CAAML accept categories : CTE/CTV/CTM/CTH  <18-12-24, Léo Viallon-Galinier> #
+    model_config = pydantic.ConfigDict(
+        validate_assignment=True,
+        extra='forbid')
+
+    test_score: int = pydantic.Field(
+        description="CT result [0-30]",
+        ge=0, le=30)
+    fracture_character: typing.Optional[typing.Literal[
+        'SDN', 'SP', 'SC', 'RES', 'RP', 'PC', 'BRK',
+        'Clean', 'Rough', 'Irregular',
+        'Q1', 'Q2', 'Q3']] = pydantic.Field(
+            None,
+            description="")
+
+
 class CTStabilityTest(_StabilityTest):
     """
     Class for CT (Compression test) stability test.
     """
+    results: typing.List[CTStabilityTestResult] = pydantic.Field(
+        [],
+        description="Successive results of a single CT test. No results mean CT31.")
+
+
+class ECTStabilityTestResult(_StabilityTestResult):
+    """
+    Class for ECT (extended column test) stability test result.
+
+    Set score=0 for ECTPV.
+    """
+    test_score: int = pydantic.Field(
+        description="CT result [0-30]",
+        ge=0, le=30)
+    propagation: bool = False
 
 
 class ECTStabilityTest(_StabilityTest):
     """
     Class for ECT (Extended column test) stability test.
     """
+    results: typing.List[ECTStabilityTestResult] = pydantic.Field(
+        [],
+        description="Successive results of a single ECT test. No results mean ECTN.")
 
 
-class PSTStabilityTest(_StabilityTest):
+class PSTStabilityTest(_StabilityTest, _StabilityTestResult):
     """
     Class for PST (Propagation Saw test) stability test.
     """
+    column_length: typing.Optional[float] = pydantic.Field(
+        None,
+        description="Length of the column used for stability test (m).")
+    cut_length: float = pydantic.Field(
+        description="The cut length necessary to start the propagation.")
+    propagation: typing.Literal['End', 'SF', 'Arr'] = pydantic.Field(
+        description="Propagation type: \n\n"
+        "- End means the fracture propagate through the end of the column,\n"
+        "- SF means slab fracture,\n"
+        "- Arr arrest of the propagation before the end of the column.")
+
+
+class ShearFrameStabilityTestResult(_StabilityTestResult):
+    """
+    Class for shear frame stability test results.
+    """
+    model_config = pydantic.ConfigDict(
+        validate_assignment=True,
+        extra='forbid')
+
+    force: float = pydantic.Field(
+        description="Failure force (N).",
+        ge=0)
+    fracture_character: typing.Optional[typing.Literal[
+        'SDN', 'SP', 'SC', 'RES', 'RP', 'PC', 'BRK',
+        'Clean', 'Rough', 'Irregular',
+        'Q1', 'Q2', 'Q3']] = pydantic.Field(
+            None,
+            description="")
 
 
 class ShearFrameStabilityTest(_StabilityTest):
     """
     Class for Shear frame stability test.
     """
+    results: typing.List[ShearFrameStabilityTestResult] = pydantic.Field(
+        [],
+        description="Successive results of a single SF test. No results mean no failure.")
