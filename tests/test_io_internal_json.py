1718c589c7a90f61e41403454b36c419ad0da509
Leo VIALLON-GALINIER <leo.viallon@meteo.fr>
2025-01-22 15:32:04 +0100

    Tests: implement test io_internal_json + bugfixes

---
+++ tests/test_io_internal_json.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import unittest
+import tempfile
+
+import snowprofile
+
+
+def gen_snowprofile():
+    sp = snowprofile.SnowProfile()
+    sp.location.name = 'Col de Porte'
+    sp.location.latitude = 45.295043
+    sp.location.longitude = 5.76559
+    import datetime
+    sp.time.record_time = datetime.datetime(2019, 12, 25, 10, 0)
+    sp.time.report_time = datetime.datetime.now()
+
+    dp = snowprofile.profiles.DensityProfile(
+        method_of_measurement="Snow Cutter",
+        quality_of_measurement="Good",
+        probed_thickness=0.03,  # 3cm cutter thickness
+        data = {'top_depth': [1.2, 1.1, 1.0, 0.50],
+                'thickness': [0.1, 0.1, 0.5, 0.5],
+                'density': [75, 100, 180, 230]}
+    )
+    sp.density_profiles.append(dp)
+    return sp
+
+
+class TestIOInternalJSON(unittest.TestCase):
+
+    def test_to_from_snowprofile(self):
+        sp = gen_snowprofile()
+
+        json = snowprofile.io.to_json(sp)
+        spn = snowprofile.io.from_json(json)
+
+        assert spn.location.name == sp.location.name, 'Names differ'
+        assert spn.time.record_time == sp.time.record_time, 'Time differs'
+        assert sp.density_profiles[0].data.equals(spn.density_profiles[0].data), 'Density profiles differ'
+
+    def test_write_read_internal_json(self):
+        sp = gen_snowprofile()
+
+        fd = tempfile.mkstemp(prefix='snowprofile-tests', suffix='.json')
+        fn = snowprofile.io.write_internal_json(sp, fd[1])
+        spn = snowprofile.io.read_internal_json(fn)
+
+        assert spn.location.name == sp.location.name, f'Names differ, see {fn}'
+        assert spn.time.record_time == sp.time.record_time, f'Time differs, see {fn}'
+        assert sp.density_profiles[0].data.equals(spn.density_profiles[0].data), f'Density profiles differ, see {fn}'
+
+        os.remove(fn)
+
+
+if __name__ == "__main__":
+    unittest.main()
